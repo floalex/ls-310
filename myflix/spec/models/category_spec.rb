3d9c0c159ab4e8eea5a1ddf3b39a2e974f963522
floalex <flo.liang@my.com>
2017-08-23 06:41:00 +0000

    Add shoulda_matchers to video and category model test

--- myflix/spec/models/category_spec.rb
+++ myflix/spec/models/category_spec.rb
@@ -1,16 +1,5 @@
 require 'rails_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: "comedies")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-  
-  it "has many videos" do
-    comedies = Category.create(name: "comedies")
-    south_park = Video.create(title: "South Park", description: "Funny video", category: comedies)
-    friends = Video.create(title: "Friends", description: "Funny video too", category: comedies)
-    expect(comedies.videos).to eq([south_park, friends])
-  end
+  it {should have_many(:videos) }
 end
--- myflix/spec/models/video_spec.rb
+++ myflix/spec/models/video_spec.rb
@@ -1,25 +1,7 @@
 require 'rails_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "monk", description: "a great video!")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-  
-  it "belongs to category" do
-    comedies = Category.create(name: "comedies")
-    south_park = Video.create(title: "South Park", description: "Funny video", category: comedies)
-    expect(south_park.category).to eq(comedies)
-  end
-  
-  it "needs to have a title" do
-    video = Video.create(description: "good video!")
-    expect(Video.count).to eq(0)
-  end
-  
-  it "needs to have a description" do
-    video = Video.create(title: "good video!")
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
