868406b1ae9724d496051466d8fd724933c6f38a
floalex <flo.liang@my.com>
2017-09-01 05:48:29 +0000

    Add Recent Videos function and test

--- myflix/spec/models/category_spec.rb
+++ myflix/spec/models/category_spec.rb
@@ -2,4 +2,38 @@ require 'rails_helper'
 
 describe Category do
   it {should have_many(:videos) }
+  
+  describe "#recent_videos" do
+    it "returns the videos in the reverse chronical order by created_at" do
+      movies = Category.create(name: "Movies")
+      alien = Video.create(title: "Alien", description: "Classic sci-fi", category: movies, created_at: 1.day.ago)
+      lala_land = Video.create(title: "La La Land", description: "Great music!", category: movies)
+      expect(movies.recent_videos).to eq([lala_land, alien])
+    end
+    
+    it "returns all the videos if there are less than 6 videos" do
+      movies = Category.create(name: "Movies")
+      alien = Video.create(title: "Alien", description: "Classic sci-fi", category: movies, created_at: 1.day.ago)
+      lala_land = Video.create(title: "La La Land", description: "Great music!", category: movies)
+      expect(movies.recent_videos.count).to eq(2)
+    end
+    
+    it "returns 6 videos if there are more than 6 videos" do
+      movies = Category.create(name: "Movies")
+      7.times { Video.create(title: "La La Land", description: "Great music!", category: movies) }
+      expect(movies.recent_videos.count).to eq(6)
+    end
+    
+    it "returns the most recent 6 videos" do
+      movies = Category.create(name: "Movies")
+      alien = Video.create(title: "Alien", description: "Classic sci-fi", category: movies, created_at: 1.day.ago)
+      6.times { Video.create(title: "La La Land", description: "Great music!", category: movies) }
+      expect(movies.recent_videos).not_to include(alien)
+    end
+    
+    it "returns an empty array if no video in that category" do
+      movies = Category.create(name: "Movies")
+      expect(movies.recent_videos).to eq([])
+    end
+  end
 end
