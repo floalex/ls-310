63ab9160d52cecb3441cb135aa3b0561ca569680
floalex <flo.liang@my.com>
2017-12-05 05:20:00 +0000

    Add admin column and controller for videos

--- myflix/app/helpers/application_helper.rb
+++ myflix/app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def options_for_video_reviews(selected=nil)
-    options_for_select((1..5).map {|num| [pluralize(num, "Star"), num]}, selected) # ['1 Star', 1]
+    options_for_select((1..5).map {|num| [pluralize(num, "Star"), num]}, selected) # ['1 Star', 1] <option value="1">1 Star</option>
+  end
+  
+  def options_for_video_categories
+    options_for_select(Category.all.map do
+      |category| [category.name, category.id]
+    end)
   end
 end
--- myflix/config/routes.rb
+++ myflix/config/routes.rb
@@ -35,4 +35,8 @@ Myflix::Application.routes.draw do
   resources :password_resets, only: [:show, :create]
   
   resources :invitations, only: [:new, :create]
+  
+  namespace :admin do
+    resources :videos, only: [:new, :create]
+  end
 end
